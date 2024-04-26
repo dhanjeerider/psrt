3ebeb3ca353e6d249d24aeea0f42552119b8d4f5
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-04-26 15:03:29 +0530

    Create Icns.js

---
+++ Icns.js
@@ -0,0 +1,6 @@
+if (window.location.href.startsWith("https://124x.blogspot.com/")) { document.getElementById("main").style.display = "block";
+} 
+else {
+document.getElementById("main").style.display = "none";
+alert("Ask admin to provide the licence for your blog t.me/dhanjeerider");
+};
